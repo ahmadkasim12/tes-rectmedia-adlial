22db35d99b48e8dafd76bedc63f9be9e6ccee1e8
ahmadkasim12 <106042415+ahmadkasim12@users.noreply.github.com>
2025-09-05 04:18:58 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,139 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Icon } from "@iconify/react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { EmblaOptionsType } from 'embla-carousel'
+import EmblaCarousel from '@/components/ui/EmblaCarousel'
+
+const IMG_PATH = [
+  "/photos/149608.jpg",
+  "/photos/f0e956568293387bc53bb514cfc5cf92.jpg",
+  "/photos/f41e76aa862cd39be5a95a940497930f.jpeg",
+];
 
 export default function Home() {
+  
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="min-h-screen">
+      <header className="px-8 py-4 flex space-x-6 items-center w-full border-2 border-b border-gray-200">
+        <h1 className="text-xl font-bold">TNT</h1>
+        <nav className="hidden md:flex space-x-6 text-gray-600">
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Portfolio</a>
+          <a href="#">Team</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+      <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen items-stretch font-sans">
+        <div className="flex flex-col justify-center space-y-8 px-8 py-12 md:px-16">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="space-y-0">
+            <p className="uppercase text-gray-500 font-semibold text-sm">Hello World!</p>
+            <h2 className="text-4xl md:text-5xl font-black tracking-tighter leading-tight mb-4">
+              We are creative agency.
+              <br/>
+              We build digital work that
+              <br/>
+              you will love!
+            </h2>
+            <Button className="text-white text-md bg-yellow-500 hover:bg-yellow-600 px-6 py-6 rounded-none">
+              Explore our portfolio →
+            </Button>
+          </div>
+
+          {/* jasa yang di offer */}
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-1 pt-8 items-end">
+            <Card className="border-t-4 bg-neutral-50 shadow-none border-red-500 border-b-0 border-l-0 border-r-0 rounded-none">
+              <CardContent className=" flex flex-col px-4 pt-4">
+                <div className="flex-1">
+                  <Icon icon="ph:desktop-light" width={40} height={40} className="text-red-500 mb-4"></Icon> 
+                  <h3 className="font-bold text-2xl leading-7 mb-2">Web Design Project</h3>
+                  <p className="text-gray-500 text-sm mb-6">
+                    Sed ut perspjctatjs unde omni
+                      iste natus error SJt volunteer
+                      accusantum doloremque.
+                  </p>
+                </div>
+                <div className="flex justify-end">
+                  <Icon icon="gg:arrow-up-o" width={35} height={35} rotate={45} className="text-red-500"></Icon>
+                  </div>
+              </CardContent>
+            </Card>
+
+            <Card className="shadow-none bg-neutral-50 rounded-none border-blue-500 border-t-4 border-b-0 border-l-0 border-r-0">
+              <CardContent className="flex flex-col px-4 pt-4">
+                <div className="flex-1">
+                  <Icon icon="solar:ruler-cross-pen-outline" width={40} height={40} className="text-blue-500 mb-4"></Icon> 
+                  <h3 className="font-bold text-2xl leading-7 mb-2">Digital Artwork</h3>
+                  <p className="text-gray-500 text-sm mb-4">
+                    Sed ut persptciatls unde omni iste natus error sit volunte-
+                  </p>
+                </div>
+                <div className="flex justify-end">
+                  <Icon icon="gg:arrow-up-o" width={35} height={35} rotate={45} className="text-blue-500"></Icon>
+                  </div>
+              </CardContent>
+            </Card>
+
+            <Card className="rounded-none shadow-none bg-neutral-50 border-green-500 border-b-0 border-l-0 border-r-0 border-t-4">
+              <CardContent className="flex flex-col px-4 pt-4">
+                <div className="flex-1">
+                  <Icon icon="hugeicons:computer-programming-01" width={40} height={40} className="text-green-500 mb-4"></Icon> 
+                  <h3 className="font-bold text-2xl leading-7 mb-2">Website Development</h3>
+                  <p className="text-gray-500 text-sm mb-10">
+                    Sed ut perspxjat•s unde omni
+                      tste natus error sit volunteer
+                      accusantum doloremque. ut
+                      perspioatjs unde omm iSte
+                      natus error Sit volunteer
+                      accusantum doloremque,
+                  </p>
+                </div>
+                <div className="flex justify-end">
+                  <Icon icon="gg:arrow-up-o" width={35} height={35} rotate={45} className="text-green-500"></Icon>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* astronaut image carousel thingy */}
+        <div className="relative bg-cyan-500 h-full">
+          <Carousel className="w-full h-full">
+            <CarouselContent>
+              {IMG_PATH.map((path, index) => (
+                <CarouselItem key={index}>
+                  <div className="relative h-screen md:h-full w-full">
+                    <Image
+                      src={path}
+                      alt={`Slide ${index + 1}`}
+                      fill
+                      style={{ objectFit: "cover" }}
+                      sizes="(max-width: 768px) 100vw, 50vw"
+                      priority
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="left-2" />
+            <CarouselNext className="right-2" />
+          </Carousel>
+
+          <div className="absolute bottom-6 left-6 text-white text-sm z-10">
+            <p className="font-light">Artwork project</p>
+            <p className="font-semibold">Enjoy the space</p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
